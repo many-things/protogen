ff2367d6c52a535ce3c54c4df356e50b0512df2d
Junho Yeo <i@junho.io>
2022-06-29 00:11:43 +0900

    Parse swagger.json

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-const EXAMPLE_URL = 'https://osmosis.stakesystems.io/swagger/swagger.yaml'
+import axios from "axios"
+import YAML from 'yaml'
+
+const EXAMPLE_GATEWAY_URL = 'https://osmosis.stakesystems.io'
+const EXAMPLE_SWAGGER_FILE_URL = `${EXAMPLE_GATEWAY_URL}/swagger/swagger.yaml`
+
+const main = async () => {
+  const { data } = await axios.get(EXAMPLE_SWAGGER_FILE_URL)
+  const swagger = YAML.parse(data)
+}
+main()
